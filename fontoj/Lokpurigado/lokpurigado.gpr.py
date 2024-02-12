033216dc2d78a7b75780c4e85c2569b05e6cff62
jmichault <jmt.2000@free.fr>
2024-02-12 21:24:49 +0100

    mise à jour des binaires

--- fontoj/Lokpurigado/lokpurigado.gpr.py
+++ fontoj/Lokpurigado/lokpurigado.gpr.py
@@ -28,7 +28,7 @@ register(
     name = _("Lokpurigado"),
     description = _("Lokpurigado helpas kompletigi lokojn uzante OpenStreetMap."),
     status = STABLE,
-    version = '1.0.29',
+    version = '1.0.31',
     gramps_target_version = '5.1',
     fname = "lokpurigado.py",
     gramplet = 'Lokpurigado',
