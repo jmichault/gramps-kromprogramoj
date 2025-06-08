fec859344a33f7a034a313069bb3e4b66810cf29
jean <48726908+jmichault@users.noreply.github.com>
2025-06-08 20:18:06 +0200

    mise à jour des binaires

--- fontoj/PlaceFrCog/placefrcog.gpr.py
+++ fontoj/PlaceFrCog/placefrcog.gpr.py
@@ -38,7 +38,7 @@ register(GRAMPLET,
          gramplet_title = _("Fr COG"),
          detached_width = 510,
          detached_height = 480,
-         version = '1.0.41',
+         version = '1.0.43',
          gramps_target_version= major_version,
          include_in_listing = True,
         )
