2cbf0803aee29285149b404a0f6755ac8c768151
jean <48726908+jmichault@users.noreply.github.com>
2025-06-16 22:57:31 +0200

    mise à jour des binaires

--- fontoj/PersonGN/PersonGN.gpr.py
+++ fontoj/PersonGN/PersonGN.gpr.py
@@ -40,7 +40,7 @@ register(GRAMPLET,
          gramplet_title=_("Geneanet"),
          detached_width = 500,
          detached_height = 500,
-         version = 'beta 0.1.6',
+         version = 'beta 0.1.7',
          gramps_target_version= major_version,
          navtypes=["Person"],
          )
